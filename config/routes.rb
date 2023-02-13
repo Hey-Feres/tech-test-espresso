8d73022f2933d20fbe011dc4c73e75416824dc19
Hey-Feres <brunoferesvillela@icloud.com>
2023-02-13 13:15:44 -0300

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #, controllers: { registrations: "registrations", sessions: "sessions", passwords: "passwords" }
   root to: "home#index"
 end
