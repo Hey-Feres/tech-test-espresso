cdf6b41c42f5bb0bd39325a320a353410e6de17b
Hey-Feres <brunoferesvillela@icloud.com>
2023-02-14 16:58:43 -0300

    Added default url host in confi/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.default_url_options[:host] = 'https://salty-basin-86770.herokuapp.com'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
